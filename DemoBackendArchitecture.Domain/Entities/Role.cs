8f83866552bd67d7b0a04e28a4ef11a1adca2929
ミ。Herniel <142606461+Sanyuri@users.noreply.github.com>
2024-08-20 15:38:16 +0700

    Add Role

---
+++ DemoBackendArchitecture.Domain/Entities/Role.cs
@@ -0,0 +1,8 @@
+﻿namespace DemoBackendArchitecture.Domain.Entities;
+
+public class Role
+{
+    public int Id { get; set; }
+    public string? RoleName { get; set; }
+    public ICollection<User> Users { get; set; } = new HashSet<User>();
+}
---
+++ DemoBackendArchitecture.Infrastructure/Mappings/RoleEntityTypeConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using DemoBackendArchitecture.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace DemoBackendArchitecture.Infrastructure.Mappings;
+
+public class RoleEntityTypeConfiguration : IEntityTypeConfiguration<Role>
+{
+    public void Configure(EntityTypeBuilder<Role> builder)
+    {
+        builder.HasKey(r => r.Id);
+        builder.Property(r => r.Id).ValueGeneratedOnAdd();
+        builder.Property(r => r.RoleName).IsRequired().HasMaxLength(50);
+        builder.HasMany<User>().WithOne().HasForeignKey(u => u.RoleId);
+    }
+}
